f497c8ead03699d1b8187a70c804fd354366c5d8
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-04 12:51:53 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,7 +14,7 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
     "Book.objects.all()"
-
+"from .models import Library"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Provide books explicitly for template convenience
