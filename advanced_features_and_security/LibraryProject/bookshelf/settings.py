bc11d092feee464cdb6746b1888d07af1a9ec31a
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-14 20:43:48 +0300

    Create settings.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/settings.py
@@ -0,0 +1,16 @@
+# This is a partial settings file. You should add this line to your project's main settings.py.
+
+# Add the 'users' app to your INSTALLED_APPS
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Your custom apps here
+    'users',
+]
+
+# Set the custom user model to be used by the authentication system
+AUTH_USER_MODEL = 'users.CustomUser'
