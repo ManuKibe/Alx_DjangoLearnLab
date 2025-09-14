d8086db457e5be06175cd466e43b2cc8afbb30b6
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-14 21:31:02 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -50,3 +50,23 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Example: extend existing model with permissions
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
