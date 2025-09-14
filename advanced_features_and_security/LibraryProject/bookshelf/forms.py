380f5128049562b547cadf434b744ffa4f015749
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-14 22:23:13 +0300

    Create forms.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    """
+    A form for the Book model to demonstrate secure input handling.
+    """
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
