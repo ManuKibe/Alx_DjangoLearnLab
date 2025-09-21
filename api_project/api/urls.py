36a7c87a328ef0ea0dcdbfb46a27506b6728329f
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-21 23:44:59 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,4 +3,12 @@ from .views import BookList
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # 🔑 This connects api/
+]
+
 ]
