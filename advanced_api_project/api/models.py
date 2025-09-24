e7b7bc3151ca08223225e9c905916327672ff340
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-24 20:27:36 +0300

    Update models.py

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
