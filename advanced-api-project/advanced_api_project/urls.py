07e74042d78c1bc6f146a3b28356b2e1fb9ee2ae
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-09-25 12:57:47 +0300

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -28,7 +28,7 @@ from .views import (
     BookUpdateView,
     BookDeleteView,
 )
-
+"api.urls"
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
@@ -36,4 +36,5 @@ urlpatterns = [
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
+
 
